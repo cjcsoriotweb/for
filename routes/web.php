97b267a7eb2c719c0390e981a78f4d2724f8aa5b
Cjcsoriot <cjcsoriot@gmail.com>
2025-10-12 00:55:54 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,3 @@ Route::middleware([
     })->name('dashboard');
 });
 
-use Illuminate\Http\Request;
-use Laravel\Jetstream\TeamInvitation;
-
-Route::get('/invites/check', function (Request $request) {
-    $email = (string) $request->query('email');
-    if (! filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        return response()->json(['has' => false]);
-    }
-    $has = TeamInvitation::where('email', $email)->exists();
-    return response()->json(['has' => $has]);
-})->name('invites.check');
