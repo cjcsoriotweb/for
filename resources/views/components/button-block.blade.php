d6b845df9375794e80f975624d2925f5316bf323
Cjcsoriot <cjcsoriot@gmail.com>
2025-10-13 13:51:52 +0200

    save

--- resources/views/components/button-block.blade.php
+++ resources/views/components/button-block.blade.php
@@ -1,21 +1,30 @@
-<div class="bg-white p-8 rounded-xl shadow-lg   transition duration-300">
-    <h3 class="font-bold text-blue-600 mb-4">{!! $titre !!}</h3>
+<div
+    class="bg-white p-8 rounded-xl shadow-lg   transition duration-300  flex flex-col ">
+    <div>
+        <h3 class="font-bold text-blue-600 mb-4">{!! $titre !!}</h3>
+        @if(isset($description))
+            <p class="text-gray-600 mb-6">
+                {!! $description !!}
+            </p>
+            <br>
+        @endif
+    </div>
 
-    @if(isset($description))
-    <p class="text-gray-600 mb-6">
-        {!! $description !!}
-    </p>
-    <br>
-    @endif
 
     @if(isset($image) && $image)
-        <img src="{{ $image }}" alt="{{ $titre }}" class="w-full h-auto rounded-lg">
+    <div class="mt-auto self-center">
+        <img style="height:100px;width: 100px;object-fit: scale-down;" src="{{ $image }}" alt="{{ $titre }}"
+            class="w-full h-auto rounded-lg">
         <br>
+    </div>
     @endif
 
 
-    <a href="{{ $url }}"
-        class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded-lg transition duration-300 inline-block">
-        {!! $button ?? 'Accéder' !!}
-    </a>
+    <div class="mt-auto self-start">
+        <a href="{{ $url }}"
+            class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded-lg transition duration-300 inline-block">
+            {!! $button ?? 'Accéder' !!}
+        </a>
+    </div>
 </div>
+
