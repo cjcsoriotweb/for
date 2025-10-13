402ea46b18c8e5d31b702252844eb00b173258fb
Cjcsoriot <cjcsoriot@gmail.com>
2025-10-13 10:42:09 +0200

    ====

--- resources/views/components/out-app-navigation-menu.blade.php
+++ resources/views/components/out-app-navigation-menu.blade.php
@@ -137,19 +137,7 @@
                         {{ __('Manage Team') }}
                     </div>
 
-                    <!-- Team Settings -->
-                    <x-responsive-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}" :active="request()->routeIs('teams.show')">
-                        {{ __('Team Settings') }}
-                    </x-responsive-nav-link>
 
-                    <!--
-                    @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
-                        <x-responsive-nav-link href="{{ route('teams.create') }}" :active="request()->routeIs('teams.create')">
-                            {{ __('Create New Team') }}
-                        </x-responsive-nav-link>
-                    @endcan
-                        -->
-                    <!-- Team Switcher -->
                     @if (Auth::user()->allTeams()->count() > 1)
                         <div class="border-t border-gray-200"></div>
 
