63d029e214e063a405020b4e1a74ab79cd432725
Cjcsoriot <cjcsoriot@gmail.com>
2025-10-15 08:51:31 +0200

    Update index.blade.php

--- resources/views/application/eleve/index.blade.php
+++ resources/views/application/eleve/index.blade.php
@@ -4,7 +4,7 @@
 
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Accueil') }} <b>{{ $team->name }}</b>
+            {{ __('Espace apprentis') }}
         </h2>
     </x-slot>
 
