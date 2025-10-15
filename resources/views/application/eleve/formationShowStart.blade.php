4602e4e74a202c87a7ebf54e6b6503feca26d28b
Cjcsoriot <cjcsoriot@gmail.com>
2025-10-15 11:58:48 +0200

    Update formationShowStart.blade.php

--- resources/views/application/eleve/formationShowStart.blade.php
+++ resources/views/application/eleve/formationShowStart.blade.php
@@ -8,4 +8,6 @@
     <div class="py-12 space-y-6">
         <p>{{ $formation->description }}</p>
     </div>
+
+    <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{{ __('Commencer cette formation') }}</button>
 </x-application-layout>
