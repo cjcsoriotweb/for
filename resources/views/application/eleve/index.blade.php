2100aa353535f679bd9b243b063b140243cbdf94
Cjcsoriot <cjcsoriot@gmail.com>
2025-10-15 11:57:36 +0200

    Update index.blade.php

--- resources/views/application/eleve/index.blade.php
+++ resources/views/application/eleve/index.blade.php
@@ -12,8 +12,6 @@
 
         <x-block-navigation :navigation="[
             ['title' => 'Formations entamées', 'description' => 'Continuez une formation', 'route' => 'application.eleve.formation.index', 'icon' => 'cog'],
-            ['title' => 'Trouver une formation', 'description' => 'Choisisez une formation', 'route' => 'application.admin.index', 'icon' => 'cog'],
-            ['title' => 'Vos documents', 'description' => 'Gérer les documents de l\'application', 'route' => 'application.admin.index', 'icon' => 'cog'],
 
         ]" card="bg-red-500" :team="$team"  back="{{ route('application.index',$team) }}" />
 
