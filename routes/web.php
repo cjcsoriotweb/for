cd3cf17f3953be6f9ea7f77b736031f4e2e1fcbd
Cjcsoriot <cjcsoriot@gmail.com>
2025-10-16 10:43:38 +0200

    save

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::get('/test', function(){
+
+    $return = "test";
+    dd($return);
+});
+
 Route::view('/', 'welcome')->name('home');
 Route::view('/policy', 'policy')->name('policy');
