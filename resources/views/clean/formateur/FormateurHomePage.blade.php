4f10257f90ef5770fa9a9aa0f4b49c64046813ef
Cjcsoriot <cjcsoriot@gmail.com>
2025-10-20 18:43:43 +0200

    Update FormateurHomePage.blade.php

--- resources/views/clean/formateur/FormateurHomePage.blade.php
+++ resources/views/clean/formateur/FormateurHomePage.blade.php
@@ -1,16 +1,6 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    Vous êtes connecté en tant que formateur !
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Lister formations -->
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             @livewire('formateur.formations.formations-list')
         </div>
     </div>
