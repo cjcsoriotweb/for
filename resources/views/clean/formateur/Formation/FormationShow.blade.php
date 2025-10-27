eaaea331a434d903a97c663161aecf1f4bf3dc07
Cjcsoriot <cjcsoriot@gmail.com>
2025-10-27 07:40:06 +0100

    Update FormationShow.blade.php

--- resources/views/clean/formateur/Formation/FormationShow.blade.php
+++ resources/views/clean/formateur/Formation/FormationShow.blade.php
@@ -24,7 +24,7 @@
         </div>
 
         <!-- Navigation Cards -->
-        <div class="mb-8">
+        <div class="mb-8 p-5">
           <h2 class="text-2xl font-bold text-gray-900 mb-6">Gestion de la formation</h2>
           <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             <!-- Edit Formation Card -->
