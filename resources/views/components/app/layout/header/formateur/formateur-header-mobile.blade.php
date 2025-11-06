d884782d4be7c89b61f752c8f05cb8978afdb05f
cjcsoriotweb <cjc.soriot@gmail.com>
2025-11-06 18:53:29 +0100

    Update formateur-header-mobile.blade.php

--- resources/views/components/app/layout/header/formateur/formateur-header-mobile.blade.php
+++ resources/views/components/app/layout/header/formateur/formateur-header-mobile.blade.php
@@ -12,7 +12,9 @@
 </button>
 
 <el-disclosure id="products" hidden class="mt-2 block space-y-2">
-    <a href="{{ route('formateur.formations.create') }}"
+    <a href="{{ route('formateur.home') }}"
+        class="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50">{{ __('Vos formations') }}</a>
+            <a href="{{ route('formateur.formations.create') }}"
         class="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50">{{ __('Nouvelle formation') }}</a>
     <a href="{{ route('formateur.import') }}"
         class="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50">{{ __('Importer formations') }}</a>
