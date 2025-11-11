8595cdbfbf4afa07df2dbccb2bd9ac85b029ad8a
cjcsoriotweb <cjc.soriot@gmail.com>
2025-11-11 14:01:30 +0100

    Update admin-home-page.blade.php

--- resources/views/in-application/admin/admin-home-page.blade.php
+++ resources/views/in-application/admin/admin-home-page.blade.php
@@ -1,9 +1,7 @@
 <x-app-layout :team="$team" icon="shield_person" :title="__('Tableau de bord administrateur')" :subtitle="__('Pilotez votre plateforme de formation avec une vue claire sur vos actions clés.')">
 
     <div class="relative pb-16">
-        <div
-            class="absolute inset-x-0 top-0 -z-10 h-72 bg-gradient-to-br from-slate-950 via-indigo-900 to-slate-900 opacity-80 blur-3xl">
-        </div>
+
         <x-admin.admin-menu-fast :team="$team" />
     </div>
 
