c1aa64249289cd57538f9444005231aacdcbe019
cjcsoriotweb <clement.julien.corentin.soriot@gmail.com>
2025-11-13 19:03:35 +0100

    Update 500.blade.php

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -2,14 +2,15 @@
 <!DOCTYPE html>
 <html lang="fr" class="h-full">
     <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>
-            500 — {{ $siteName ?? config("app.name", "Application") }}
-        </title>
-
-        <x-meta-header />
-
+  <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>{{ config('app.name') }}</title>
+  <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  @livewireStyles
+  @stack('head')
         <meta name="robots" content="noindex,follow" />
     </head>
     <body class="h-full bg-white text-gray-900">
